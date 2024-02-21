17086a5e6a17ed1d07300dff10edca4a57f0f4b5
Andy Woodruff <awoodruff@gmail.com>
2024-02-21 10:23:25 -0500

    Simple image carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,34 @@ function randomizeShops() {
 }
 
 randomizeShops();
+
+
+document.querySelectorAll('button.carousel-control-button').forEach(btn => {
+    btn.addEventListener('click', e => {
+        e.preventDefault();
+        const images = Array.from(e.target.closest('.carousel').querySelectorAll('img'));
+        const dots = Array.from(e.target.parentNode.querySelectorAll('.image-dot'));
+        const activeIndex = images.findIndex(img => img.classList.contains('active'));
+        images[activeIndex].classList.remove('active');
+        dots[activeIndex].classList.remove('active');
+        let nextIndex = e.target.classList.contains('next') ? activeIndex + 1 : activeIndex - 1;
+        if (nextIndex >= images.length) nextIndex = 0;
+        if (nextIndex < 0) nextIndex = images.length - 1;
+        images[nextIndex].classList.add('active');
+        dots[nextIndex].classList.add('active');
+    })
+});
+
+document.querySelectorAll('button.image-dot').forEach(btn => {
+    btn.addEventListener('click', e => {
+        e.preventDefault();
+        const images = Array.from(e.target.closest('.carousel').querySelectorAll('img'));
+        const dots = Array.from(e.target.parentNode.querySelectorAll('.image-dot'));
+        const activeIndex = images.findIndex(img => img.classList.contains('active'));
+        images[activeIndex].classList.remove('active');
+        dots[activeIndex].classList.remove('active');
+        const nextIndex = dots.indexOf(e.target);
+        images[nextIndex].classList.add('active');
+        dots[nextIndex].classList.add('active');
+    })
+});
